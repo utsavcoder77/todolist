d273d379e9e2ef472f57a474e1933450ba02b072
utsavcoder77 <baralutsav89@gmail.com>
2024-03-05 19:34:19 +1100

    added if-else statement in js [utsav]

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,77 @@
 (() => {
     const allItems = [];
+
+    function populateTotalItems (count) {
+        const totalElement = document.getElementById("total");
+        totalElement.innerText = count;
+    }
+
     function addNewItem(event) {
         event.preventDefault();
 
-
         const inputElement = document.querySelector("input");
-        const newItem = inputElement.value;
-       
+        let newItem = inputElement.value;
+    
         if(newItem) {
-        allItems.push(newItem);
-        populateItems(allItems);
-        inputElement.value = "";
+            allItems.unshift(newItem);
+            populateItems(allItems.slice(0, 5));
+            populateTotalItems(allItems.length);
+            inputElement.value = "";
         }
-        
-
+        else {
+            inputElement.classList.add("error-input");
+        }
+        toggleViewButton();
     }
+
 
     function populateItems(allItems) {
         const listsElement = document.getElementsByClassName("lists")[0];
         
-        const allElements = [];
+        const allElements = [] ;
         allItems.forEach((item) => {
             allElements.push(`<div>
                 <span>${item}</span>
-                 <input type="checkbox">
-             </div>`);
+                <input type="checkbox" >
+            </div>`);
         });
         listsElement.innerHTML = allElements.join("");
     }
 
+    
 
+    function showAllItems(event) {
+        populateItems(allItems);
+        if (event.target.innerText === "View All") {
+            populateItems(allItems);
+            event.target.innerHTML = "View Less"
+        }
+        else {
+            populateItems(allItems.slice(0, 5));
+            event.target.innerHTML = "View All";
+        }
+    }
+
+    function toggleViewButton() {
+        if (allItems.length > 5) {
+            document.getElementById("view-btn").style.display = "inline";
+        }
+
+        else{
+            document.getElementById("view-btn").style.display = "none";
+        }
+        
+    }
+
+
+
+
+    
+    document.querySelector("input").addEventListener("focus", () => {
+        const inputElement = document.querySelector("input");
+        inputElement.classList.remove("error-input");
+    });
     document.getElementById("add_new_btn").addEventListener("click", addNewItem);
+    document.getElementById("view-btn").addEventListener("click", showAllItems);
+    toggleViewButton();
 })();
